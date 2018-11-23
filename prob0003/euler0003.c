863f0dcf57796cd3fd53356488dd2585dfb825fc
npfasoformoso <ffhaon2002@gmail.com>
2018-11-22 20:08:45 -0900

    Solved Prob 3.

---
+++ prob0003/euler0003.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+	long int euler = 600851475143;
+	int i = 2, largestFactor;
+	printf("600851475143 = 1");
+	while (euler != 1){
+		if (euler % i == 0){
+			largestFactor = i;
+			euler /= i;
+			printf(" * %i", largestFactor);
+		}
+		i++;
+	}
+	printf("\nLargest Prime Factor: %i\n", largestFactor);
+	return 0;
+}
