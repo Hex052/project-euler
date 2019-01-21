015249cad111fbc9707447de67f1e73d35186683
Hex052 <elijahiff@gmail.com>
2019-01-20 15:29:52 -0900

    Start of idea for problem

---
+++ prob0026/FractionDigitCycles.java
@@ -0,0 +1,16 @@
+package prob0026;
+
+import general.Array;
+import general.Factor;
+import java.util.Arrays;
+public class FractionDigitCycles {
+	public static void main(String[] args) {
+		int longest = 7, len = 6, currlen;
+		int[] div = null;
+		for (int i = 2; i < 1000; i++) {
+			div = new int[i];
+			currlen = 0;
+
+		}
+	}
+}
