b68dec59b72d46d4c41df21fb556462c52fccd8b
Hex052 <elijahiff@gmail.com>
2019-08-28 20:59:58 -0800

    Fix stopwatch in template

--- template.cpp
+++ template.cpp
@@ -3,21 +3,18 @@
 #include <chrono>
 #include <vector>
 
-using namespace std;
-
 //prototypes
 //bool is_pandigital(string const &str);
 
 int main(int argc, char const *argv[]) {
-	auto start_time = chrono::high_resolution_clock::now();
+	auto start_time = std::chrono::high_resolution_clock::now();
 	//Start stopwatch
 
 
 
 	//Stop stopwatch
-	auto stop_time = chrono::high_resolution_clock::now();
-	auto nanos = chrono::duration_cast<chrono::nanoseconds>(stop_time - start_time);
-	cerr << "\nExecution took " << nanos.count() << " ns\n";
-
+	auto stop_time = std::chrono::high_resolution_clock::now();
+	auto nanos = std::chrono::duration_cast<std::chrono::nanoseconds>(stop_time - start_time);
+	std::cerr << "\nExecution took " << nanos.count() << " ns\n";
 	return 0;
 }
